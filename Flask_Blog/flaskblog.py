ba765c03db34be6c16fe41eaca28c4a42aa62367
oreanroy <oreanroy@gmail.com>
2019-06-19 18:17:18 +0530

    Login and register page added

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistraionForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
@@ -25,6 +28,27 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistraionForm()
+    if form.validate_on_submit():
+        flash('Account cretaed for user, {} !'.format({form.username.data}), 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == '123':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Your login was nor succesful', 'danger')
+            return redirect(url_for('login'))
+    return render_template('login.html', title='Login', form=form)
+
 
 
 if __name__ == "__main__":
